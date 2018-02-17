e8caf5717f78c721f43cd4d0e5f60a3f3dd80820
joestephens <joe.stephens@live.co.uk>
2018-02-17 15:15:53 +0000

    Parse fields and input to db

--- parser.js
+++ parser.js
@@ -1,5 +1,9 @@
+require('dotenv').config()
 const fs = require('fs')
 const parse = require('csv-parse')
+const mongojs = require('mongojs')
+const db = mongojs(process.env.DB_URL)
+const people = db.collection('people')
 
 fs.readFile('./speakers2.csv', function (error, data) {
   if (error) {
@@ -10,6 +14,55 @@ fs.readFile('./speakers2.csv', function (error, data) {
     if (error) {
       throw error
     }
-    console.log(data)
+
+    data.forEach(function (person) {
+      for (field in person) {
+        if (person[field] && typeof person[field] === 'string') {
+          person[field] = person[field].trim()
+        }
+
+        if (field === 'twitter') {
+          person['twitter'] = person['twitter'].replace('@', '')
+        }
+
+        if (field === 'interests') {
+          person['interests'] = person['interests'].split(/[.:;?!~,`"&|()<>{}\[\]\r\n/\\]+/)
+            .map(interest => interest.trim())
+            .filter(interest => interest)
+        }
+
+        if (field === 'skills') {
+          person['skills'] = person['skills'].split(/[.:;?!~,`"&|()<>{}\[\]\r\n/\\]+/)
+          .map(skill => skill.trim())
+          .filter(skill => skill)
+        }
+
+        if (field === 'industries') {
+          person['industries'] = person['industries'].split(/[.:;?!~,`"&|()<>{}\[\]\r\n/\\]+/)
+          .map(industry => industry.trim())
+          .filter(industry => industry)
+        }
+
+        if (field === 'companies') {
+          person['companies'] = person['companies'].split(/[.:;?!~,`"&|()<>{}\[\]\r\n/\\]+/)
+          .map(company => company.trim())
+          .filter(company => company)
+        }
+      }
+
+      people.findOne({ firstName: data.firstName, lastName: data.lastName }, function (error, doc) {
+        if (error) {
+          throw new Error(error)
+        }
+
+        if (doc) {
+          console.log('Omitting record - possible duplicate - add manually')
+          return console.log(person)
+        }
+  
+        people.insert(data)
+      })
+    })
+
   })
 })
